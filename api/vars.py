664d0aa4073b2eb598b987122c07061c9b58010f
Ekky Armandi <ekkyarmandi@gmail.com>
2025-04-07 05:57:13 +0700

    feat: expand REID_CODE and add ZONING_COLORS and ZONING_CATEGORIES dictionaries

--- api/vars.py
+++ api/vars.py
@@ -1,3 +1,47 @@
-REID_CODE = []
-ZONING_COLORS = []
-ZONING_CATEGORIES = []
+# REID Websites and their codes
+REID_CODE = {
+    "Bali Properties for Sale": "BOFS",
+    "Teal Estate": "TEST",
+    "Bali Property Direct": "BPOD",
+    "Bali Real Estate Consultants": "BREC",
+    "Bali Realty": "BREL",
+    "Bali Select": "BSEL",
+    "Bali Treasure Properties": "BTPR",
+    "Heritage Bali": "HRTB",
+    "Unreal Bali": "URLB",
+    "Exotiq Property": "EXCP",
+    "Kibarer": "KIBR",
+    "Paradise Property Group": "PPGB",
+    "Lazudi": "LAZD",
+    "Suasa Real Estate": "SURE",
+    "Svaha Property": "SVHP",
+    "Luxindo Property": "LUXP",
+    "Raja Villa Property": "RJVP",
+    "GD&ASSOCIATES": "GDAC",
+    "Bali Home Immo": "BHIM",
+    "Propertia": "PROP",
+    "Bali Exception": "BEXC",
+    "Villas of Bali": "VOFB",
+    "Dot Property": "DOTP",
+    "Bali Coconut Living": "BCLV",
+    "Ray White Indonesia": "RWID",
+    "Bali Moves": "BLMV",
+    "Ubud Property": "UBPR",
+}
+
+ZONING_COLORS = {
+    "yellow": "Residential",
+    "red": "Commercial",
+    "pink": "Tourism",
+    "green": "Agricultural",
+    "dark green": "Green",
+    "orange": "Sacred",
+    "grey": "Industrial",
+    "blue": "Special",
+}
+
+ZONING_CATEGORIES = {
+    "residential": "Residential",
+    "commercial": "Commercial",
+    "tourism": "Tourism",
+}
